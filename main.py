bfb09700e84eede1af4ef45a5300a9fc7411f38e
Edwin-George101 <edwnslife@gmail.com>
2025-01-16 23:43:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 print("Welcome to the food ordering ChatBot.")
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you?")
+
+print("How may we help you?")
+print(" ")
+print("Please select one from the following")
+
+one  = print("1. I'd like to collect some change.")
+two = print("2. I was not satisfied with my care.")
+three = print("3. I would like to leave a positive review.")
+four = print("4. I would like to speak to your manager.")
+
+print("\nWhat would you like to do?")
