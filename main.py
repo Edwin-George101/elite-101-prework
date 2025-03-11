87ad828f9bad8702713e2a8ed3564bb9febdc9a6
Edwin-George101 <edwnslife@gmail.com>
2025-03-11 17:09:15 +0000

    edit#	modified:   main.py

--- main.py
+++ main.py
@@ -10,8 +10,24 @@ print("Please select one from the following:")
 
 def charity():
     responses = ["Every dollar counts! Thank you so much.", "We appreciate your generosity here at Code2College!", "Thank you for your contribution. We will put your name on the wall!"]
-    print(random.charity(responses))
+    print(random.choice(responses))
 
+def not_satisfied():
+    responses = ["We are so sorry to hear that. We will try to do better.", "Your feedback is so valuable to us! Please write a review and tell us what went wrong.", "We apologize for any inconveniences we caused."]
+    print(random.choice(responses))
+
+def leave_review():
+    print("We are so happy that you want to leave a positive review!")
+    site = input("Please type 'Google', 'Yelp', or 'Facebook': ").strip().lower()
+    if site == "google":
+        print("Great choice! Leave a review here at www.google.com/reviews")
+    elif site == "yelp":
+        print("Awesome! Leave a rewview here at www.yelp.com/reviews")
+    elif site == "facebook":
+        print("Wonderful! Leave a review here at www.facebook.com/reviews")
+    else:
+        print("You have either inputted the wrong website or did not want to leave a review. Please try again!")
+    
 print("1. I'd like to donate for your cause!")
 print("2. I was not satisfied with my care.")
 print("3. I would like to leave a positive review.")
@@ -23,9 +39,9 @@ while True:
     if choice == "1":
         charity()
     elif choice == "2":
-        print("What can we do to make things better?")
+        not_satisfied()
     elif choice == "3":
-        print("Thank you so much! ")
+        leave_review()
     elif choice == "4":
         print("I understand. They will be with you shortly.")
     elif choice == "5":
