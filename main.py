9bb0a1db767baeb6520a5faa3d5ce8fba344fdb6
Edwin-George101 <edwnslife@gmail.com>
2025-03-11 17:23:10 +0000

    18

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print("Welcome to the food ordering ChatBot.")
 name = input("What is your name? ")
 age = int(input("Hello " + name + ", how old are you? "))
 
-while age<=18:
+while age<18:
     age_choice = input("Please find a parent or guardian to assist you in these issues. Are they with you now? Please type 'Yes' or 'No'").strip().lower()
     if age_choice =='yes':
         age = 18
